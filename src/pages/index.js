945817fa14a618b26372b0cd839aa568b4f86b61
Muhammad Jamshaid Ghaffar <muhammadjamshaid89@gmail.com>
2022-10-07 00:42:46 +0500

    removed apiUrl from init of netlify indenity

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import * as React from "react";
 import netlifyIndetity from "netlify-identity-widget";
 
 export default function () {
-  netlifyIndetity.init({ APIUrl });
+  netlifyIndetity.init({});
   netlifyIndetity.open();
   return <div>Hello World</div>;
 }
